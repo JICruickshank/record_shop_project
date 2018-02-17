d4f539f7b9ebe1e978d8c69f37a309889ef06a19
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-02-17 20:26:18 +0000

    adds seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
+require_relative("../models/artists.rb")
+require_relative("../models/albums.rb")
+
 artist1 = Artist.new({'name' => 'E-Z Rollers'})
 artist2 = Artist.new({'name' => 'Bad Company UK'})
+album1 = Album.new({'title' => 'Walk This Land', 'genre' => 'Drum \'n\' Bass', 'quantity' => 5, 'stock_level' => 'Medium' })
+album2 = Album.new({'title' => 'The Nine', 'genre' => 'Drum \'n\' Bass', 'quantity' => 10, 'stock_level' => 'High' })
