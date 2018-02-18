65bd5504f3ee27ea6ff60929bd71d34daa087999
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-02-18 21:41:15 +0000

    adds function to add album to inventory

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -1,11 +1,35 @@
-get "/add_album" do
+get "/artists/:id/add_album" do
+  @artist = Artist.find_by_id(params[:id])
   erb(:"albums/new")
 end
 
-# can't create album from album form as don't have an artist id. get array of artists, check if artist name is the same and retrive id and set as artist_id if so. Else create new artist, retrieve id number, create new album with artist_id.
-
-post "/albums/new" do
+post "/artists/:id/album_added" do
+  params[:artist_id] = params[:id]
   @album = Album.new(params)
   @album.save
   erb(:"albums/create")
+
+end
+
+get "/add_album" do
+  erb(:"albums/new_artist_and_album")
+end
+
+post "/add_album" do
+  if Artist.artist_already_exists(params[:artist_name]) == false
+    new_artist = Hash.new
+    new_artist['name'] = params[:artist_name]
+    @artist = Artist.new(new_artist)
+    @artist.save
+    params[:artist_id] = @artist.id
+    @album = Album.new(params)
+    @album.save
+    erb(:"albums/create")
+  else
+    params[:artist_id] = Artist.artist_already_exists(params[:artist_name])
+    @album = Album.new(params)
+    @album.save
+    erb(:"albums/create")
+  end
+
 end
